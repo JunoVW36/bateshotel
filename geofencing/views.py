1306b0d6dbb6dca7193dcfac38ed2026646de092
Kevin Sarmiento <kevin.sarmiento@uruit.com>
2018-06-15 11:48:04 -0500

    Adding polygon list and visualization.

--- geofencing/views.py
+++ geofencing/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
-
+from django.http import JsonResponse
+from django.shortcuts import render
+import os, json
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the geofence index.")
+    context = {}
+    return render(request, 'geofencing/index.html', context)
+
+def get_polygons(request):
+    polygons_file = open(os.path.dirname(os.path.realpath(__file__)) + '/data/polygons.json')
+    response = json.loads(polygons_file.read())
+    polygons_file.close()
+    return JsonResponse(response, safe=False)
